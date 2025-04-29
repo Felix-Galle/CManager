50274eb760333a315ec71e95c180801d746a5613
Felix-Galle <felixjohngalle@gmail.com>
2025-04-29 08:27:16 +0200

    made Cclient use winForms also

--- CClient/Program.cs
+++ CClient/Program.cs
@@ -1,34 +1,16 @@
-﻿using System;
-using System.Windows.Forms;
-using System.Drawing;
+namespace Cclient;
 
-namespace CClient
+static class Program
 {
-    static class Program
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
     {
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            NotifyIcon trayIcon = new NotifyIcon
-            {
-                Text = "CClient Running",
-                Icon = SystemIcons.Application, // You can replace with custom .ico
-                Visible = true
-            };
-
-            ContextMenuStrip contextMenu = new ContextMenuStrip();
-            contextMenu.Items.Add("Exit", null, (s, e) =>
-            {
-                trayIcon.Visible = false;
-                Application.Exit();
-            });
-
-            trayIcon.ContextMenuStrip = contextMenu;
-
-            Application.Run(); // Keeps app alive without window
-        }
-    }
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }    
 }
--- CServer/obj/Debug/net8.0-windows/CServer.AssemblyInfo.cs
+++ CServer/obj/Debug/net8.0-windows/CServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a1feb06c56f3e5e894f5e8a60e4a39f0d96c6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2644f13240456515d0d419a1c05094767823175")]
 [assembly: System.Reflection.AssemblyProductAttribute("CServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
